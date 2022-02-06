449904ded88a0bfd94685375b95abd974f239ab2
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-02-06 10:20:06 -0500

    Remove unused variables from NavigationProbe

--- Plugin/Navigation/NavigationProbe.cs
+++ Plugin/Navigation/NavigationProbe.cs
@@ -7,15 +7,12 @@ namespace PassivePicasso.RainOfStages.Plugin.Navigation
     [ExecuteAlways]
     public class NavigationProbe : MonoBehaviour
     {
-        private static Mesh mesh;
         public Color navigationProbeColor = Color.green;
 
         public int seed = 1;
         [SerializeField]
         public List<Vector3> nodePositions = new List<Vector3>();
         public float distance = 15;
-        public float minimumSurfaceDistance = 2f;
-        public float maximumSurfaceDistance = 30f;
         public float linkDistance = 10;
         public float nodeSeparation = 5;
         public int targetPointCount = 100;
@@ -37,10 +34,6 @@ namespace PassivePicasso.RainOfStages.Plugin.Navigation
         private float lastNodeSeparation = 5;
         [SerializeField, HideInInspector]
         private int lastTargetPointCount = 5;
-        [SerializeField, HideInInspector]
-        public float lastMinimumSurfaceDistance = 2f;
-        [SerializeField, HideInInspector]
-        public float lastMaximumSurfaceDistance = 30f;
 
         public void Update()
         {
@@ -52,8 +45,6 @@ namespace PassivePicasso.RainOfStages.Plugin.Navigation
               || lastNodeSeparation != nodeSeparation
               || lastTargetPointCount != targetPointCount
               || lastSeed != seed
-              || lastMinimumSurfaceDistance != minimumSurfaceDistance
-              || lastMaximumSurfaceDistance != maximumSurfaceDistance
               )
             {
                 lastPosition = transform.position;
@@ -63,8 +54,6 @@ namespace PassivePicasso.RainOfStages.Plugin.Navigation
                 lastLinkDistance = linkDistance;
                 lastNodeSeparation = nodeSeparation;
                 lastTargetPointCount = targetPointCount;
-                lastMinimumSurfaceDistance = minimumSurfaceDistance;
-                lastMaximumSurfaceDistance = maximumSurfaceDistance;
                 lastSeed = seed;
                 isDirty = true;
             }
