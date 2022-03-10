e9ea600a9b3ae0de1e3ef82100cf804fdcda2697
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-03-10 14:33:23 -0500

    Add tooltips and headers to SceneDefinition

--- Plugin/Proxy/SceneDefinition.cs
+++ Plugin/Proxy/SceneDefinition.cs
@@ -1,11 +1,16 @@
 using RoR2;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace PassivePicasso.RainOfStages.Proxy
 {
     public class SceneDefinition : SceneDef
     {
+        [Header("Stage Weaving")]
+        [Tooltip("Add this stages to other stages variants collection")]
         public List<SceneDefReference> reverseSceneNameOverrides;
+
+        [Tooltip("Add this stages to other stages destinations")]
         public List<SceneDefReference> destinationInjections;
     }
 }
