6a5cfc235ff409d28c98672b57109ee6924c3652
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-03-17 15:37:14 -0400

    update version in constants

--- Plugin/Constants.cs
+++ Plugin/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         public const string GuidName = "com.PassivePicasso.RainOfStages";
-        public const string Version = "4.1.16";
+        public const string Version = "4.1.19";
         public const string Build = ".1";
         public const string Name = "RainOfStages";
         public const string DependencyName = "Twiner-" + Name + "-" + Version;
